b92dff3fa5381e1f4ec8800c9f4007e244d0d65c
Youssef Walid <yousefwalid98@hotmail.com>
2021-05-12 01:35:42 +0200

    Changed name of coach to trainer in user auth

--- app/models/user_auth.rb
+++ app/models/user_auth.rb
@@ -2,7 +2,7 @@ class UserAuth < ApplicationRecord
   has_secure_password
   
   # Enums
-  enum role: [:user, :admin, :coach, :nutritionist]
+  enum role: [:user, :admin, :trainer, :nutritionist]
 
   # Validations
   validates: :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP } 
