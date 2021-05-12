084b2616c0ed7b783ae3ca966973040861f2a4cd
Youssef Walid <yousefwalid98@hotmail.com>
2021-05-12 02:26:58 +0200

    Added user_auth associations

--- app/models/user_auth.rb
+++ app/models/user_auth.rb
@@ -1,5 +1,9 @@
 class UserAuth < ApplicationRecord
   has_secure_password
+  has_many :admins
+  has_many :clients
+  has_many :trainers
+  has_many :nutritionists
   
   # Enums
   enum role: [:user, :admin, :trainer, :nutritionist]
