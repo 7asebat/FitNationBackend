2b5f2dcb83a2f8fb8e311904ad9fe8fb6dedd02d
Youssef Walid <yousefwalid98@hotmail.com>
2021-05-13 22:48:34 +0200

    Added clients signup and signin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  match '*unmatched_route', :to => 'errors#routing', via: [:get, :post, :delete, :put, :patch]
+
+  scope :authentication do
+    post "clients/sign_up", to: "clients#sign_up"
+    post "sign_in", to: "authentication#sign_in"
+  end
+
+  match "*unmatched_route", :to => "errors#routing", via: [:get, :post, :delete, :put, :patch]
 end
