9410bd61695c646d844dca336d2de8455ca304ce
Youssef Walid <yousefwalid98@hotmail.com>
2021-05-13 23:10:02 +0200

    Added signup for trainers

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   scope :authentication do
-    post "clients/sign_up", to: "clients#sign_up"
+    scope :clients do
+      post "sign_up", to: "clients#sign_up"
+    end
+
+    scope :trainers do
+      post "sign_up", to: "trainers#sign_up"
+    end
+
     post "sign_in", to: "authentication#sign_in"
   end
 
