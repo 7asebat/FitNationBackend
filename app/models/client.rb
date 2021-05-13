9e273f8f42280b83be98fd54a35cfe3fcb1e3793
Youssef Walid <yousefwalid98@hotmail.com>
2021-05-13 22:47:19 +0200

    Added attributes decorator for clients

--- app/models/client.rb
+++ app/models/client.rb
@@ -4,4 +4,15 @@ class Client < ApplicationRecord
   has_many :clients_weights_nutritions
   has_many :clients_exercise_instances
   has_many :workout_plans
+
+
+  def decorate
+    return {
+      id: self.id,
+      name: self.name,
+      country: self.country,
+      email: self.user_auth.email,
+      role: self.user_auth.role
+    }
+  end
 end
