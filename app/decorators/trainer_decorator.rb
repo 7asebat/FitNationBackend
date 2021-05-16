096d882e3d7397a385ea839fbe54dcbb2bd361a8
Youssef Walid <yousefwalid98@hotmail.com>
2021-05-16 04:07:21 +0200

    Added decorators for nutritionists and trainers

---
+++ app/decorators/trainer_decorator.rb
@@ -0,0 +1,13 @@
+class TrainerDecorator < Draper::Decorator
+  delegate_all
+
+  def as_json
+    {
+      id: id,
+      name: name,
+      country: country,
+      email: user_auth&.email,
+      role: user_auth&.role
+    }
+  end
+end
