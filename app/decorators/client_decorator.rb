0d312bd64df9e3257dcffb57f872305daab87515
Ahmed Hisham <ahisham360@gmail.com>
2021-06-07 15:53:05 +0200

    Populated client active workout plan

--- app/decorators/client_decorator.rb
+++ app/decorators/client_decorator.rb
@@ -1,6 +1,5 @@
 class ClientDecorator < Draper::Decorator
   delegate_all
-
   def as_json
     {
       id: id,
@@ -9,7 +8,7 @@ class ClientDecorator < Draper::Decorator
       email: user_auth&.email,
       role: user_auth&.role,
       avatar: avatar.url,
-      active_workout_plan: active_workout_plan
+      active_workout_plan: active_workout_plan.decorate.as_json_detailed_no_client
     }
   end
 end
--- app/decorators/workout_plan_decorator.rb
+++ app/decorators/workout_plan_decorator.rb
@@ -28,4 +28,15 @@ class WorkoutPlanDecorator < Draper::Decorator
       exercise_instances: object&.workout_plan_exercises.decorate.as_json.group_by{ |u| u[:day] }
     }
   end
+  def as_json_detailed_no_client
+    {
+      id: id,
+      created_at: created_at,
+      updated_at: updated_at,
+      name: name,
+      level: level,
+      requires_equipment: requires_equipment,
+      exercise_instances: object&.workout_plan_exercises.decorate.as_json.group_by{ |u| u[:day] }
+    }
+  end
 end
