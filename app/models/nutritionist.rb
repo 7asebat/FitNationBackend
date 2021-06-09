ebec791300a5b8959ef6a84f61faf2a551673bcc
Youssef Walid <yousefwalid98@hotmail.com>
2021-06-09 14:53:52 +0200

    Fix Nutritionist avatar not returning

--- app/models/nutritionist.rb
+++ app/models/nutritionist.rb
@@ -3,14 +3,4 @@ class Nutritionist < ApplicationRecord
   belongs_to :user_auth
 
   has_many :recipes
-
-  def decorate
-    return {
-      id: self.id,
-      name: self.name,
-      country: self.country,
-      email: self.user_auth.email,
-      role: self.user_auth.role
-    }
-  end
 end
